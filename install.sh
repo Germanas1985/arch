ebd8a82871bf7097534af66fefc83e1d6d4a2411
Germanas1985 <51878001+Germanas1985@users.noreply.github.com>
2021-12-12 13:40:25 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#----------------------------------
+#   █████╗ ██████╗  ██████╗██╗  ██╗
+#  ██╔══██╗██╔══██╗██╔════╝██║  ██║
+#  ███████║██████╔╝██║     ███████║
+#  ██╔══██║██╔══██╗██║     ██╔══██║
+#  ██║  ██║██║  ██║╚██████╗██║  ██║
+#  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+#----------------------------------
+    bash preinstall.sh
+    arch-chroot /mnt /root/arch/1-setup.sh
+    source /mnt/root/arch/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch/2-user.sh
+    arch-chroot /mnt /root/arch/3-post-setup.sh
